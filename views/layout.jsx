a74af3e26f05205fec2fa2fdc39a7dc127194da6
Christophe Eo Siang Ee <aragonlotr5@gmail.com>
2019-06-08 22:40:00 +0800

    fixed the footer staying at the bottom using css instead of bootstrap

--- views/layout.jsx
+++ views/layout.jsx
@@ -13,7 +13,8 @@ class Layout extends React.Component {
                     <title>{this.props.title}</title>
                 </head>
                 <body>
-
+                <div id="page-container">
+                    <div id="content-wrap">
                     <header>
                         <nav className="navbar sticky-top navbar-expand-lg navbar-light" id = "main-navbar">
 
@@ -73,7 +74,7 @@ class Layout extends React.Component {
                     </header>
 
                             {this.props.children}
-
+                    </div>
                     <footer id="footer" className="clearfix">
                         <div className ="container">
                             <ul className="list-unstyled list-inline text-center">
@@ -85,7 +86,7 @@ class Layout extends React.Component {
                             </ul>
                         </div>
                     </footer>
-
+                    </div>
                     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
                     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
                     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
