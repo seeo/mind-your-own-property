4feeddd1fcec66aaa961b43735f7179f16568c02
Christophe Eo Siang Ee <aragonlotr5@gmail.com>
2019-06-10 20:04:14 +0800

    seed.sql: tidied the file and removed user details

--- data/seed.sql
+++ data/seed.sql
@@ -1,80 +1,42 @@
-INSERT into users (
-    username, password, email
-)
-VALUES (
-    'chris',
-    '67552168',
-    'aragonlotr5@gmail.com'
-);
+-- INSERT into
+-- 	users (username, password, email)
+-- VALUES
+-- 	('chris', '67552168', 'aragonlotr5@gmail.com');
+
+-- INSERT into
+--   users (username, password, email)
+-- VALUES
+--   ('doris','91317190', 'djnlee@outlook.com');
 
 INSERT into
-  users (username, password, email)
+  properties (name, address, photo_url, rental_mth, day_credit, bank_name, user_id)
 VALUES
-  (
-    'doris',
-    '91317190',
-    'djnlee@outlook.com'
-  );
-
-INSERT into
-  properties (
-    name,
-    address,
-    photo_url,
-    rental_mth,
-    day_credit,
-    bank_name,
-    user_id
-  )
-VALUES
-  (
-    'Livia',
-    '77 Pasir Ris Grove',
-    'https://sg2-cdn.pgimgs.com/projectnet-project/5961/ZPPHO.103849195.R800X800.jpg',
+  (	'Livia',
+		'77 Pasir Ris Grove',
+		'https://sg2-cdn.pgimgs.com/projectnet-project/5961/ZPPHO.103849195.R800X800.jpg',
     2600,
     15,
     'OCBC',
-    1
-  );
+    1);
 
-  INSERT into
-  properties (
-    name,
-    address,
-    photo_url,
-    rental_mth,
-    day_credit,
-    bank_name,
-    user_id
-  )
+INSERT into
+  properties (name, address, photo_url, rental_mth, day_credit, bank_name, user_id)
 VALUES
-  (
-    'Estuary',
-    'Yishun',
-    '',
-    2000,
-    10,
-    'OCBC',
-    1
-  );
+  (	'Estuary',
+		'Yishun',
+		'https://www.asiaone.com/sites/default/files/health/20150708_Estuary_LW_0_0.jpg',
+		2000,
+		10,
+		'OCBC',
+		1);
 
-  INSERT into
-  properties (
-    name,
-    address,
-    photo_url,
-    rental_mth,
-    day_credit,
-    bank_name,
-    user_id
-  )
+INSERT into
+  properties (name, address, photo_url, rental_mth, day_credit, bank_name, user_id)
 VALUES
-  (
-    'Glendale Park',
+  (	'Glendale Park',
     'Hillview',
-    '',
+    'https://glendalepark.sg/a/2549/img/mo-glendalepark.jpg',
     2700,
     5,
     'UOB',
-    2
-  );
+    2);
